4f159a16153dccd7c07bfa15f5a4ee72d61910e6
TbI4uHKa <ju789ooll@yandex.ru>
2021-02-15 22:27:40 +0600

    Рандомные цвета

--- main.py
+++ main.py
@@ -1,27 +1,55 @@
 import sys
-from PyQt5 import QtWidgets
 from PyQt5.QtGui import QPainter, QColor
 import random
 from PyQt5 import uic
+from PyQt5 import QtCore, QtWidgets
 
 
-class Example(QtWidgets.QMainWindow):
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(589, 547)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(230, 402, 111, 61))
+        self.pushButton.setObjectName("pushButton")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 589, 21))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "Кнопка"))
+
+
+class Example(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.setupUi(self)
+
         self.qp = QPainter()
         self.initUI()
         self.flag = ''
         self.x = 300
         self.y = 200
-        self.colors = ['yellow']
+        self.colors = ['yellow', 'red', 'blue', 'green']
         self.pushButton.clicked.connect(self.run)
 
     def run(self):
         self.update()
 
     def initUI(self):
-        self.setWindowTitle('Draw text')
+        self.setWindowTitle('Draw')
         self.show()
 
     def paintEvent(self, event):
